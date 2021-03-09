dcc6b05e162b6cb97f6979222d23a078a880801f
Mylos97 <57527830+Mylos97@users.noreply.github.com>
2021-03-09 15:59:51 +0100

    first commit

---
+++ GUI.py
@@ -0,0 +1,104 @@
+from Weather import *
+import npyscreen
+
+
+
+
+class App(npyscreen.NPSAppManaged):
+    def onStart(self):
+        #add forms to the application
+        self.addForm('MAIN', FirstForm, name="main")
+        self.addForm('SECOND', SecondForm, name="second")
+    
+    def change_form(self, name):
+        self.switchForm(name)
+        self.resetHistory()
+
+
+class FirstForm(npyscreen.ActionFormMinimal):
+    def create(self):
+        self.add(npyscreen.TitleText, w_id="message", name= "How's the weather today?",editable = False, begin_entry_at= 12, relx = 2)
+
+        self.add(npyscreen.ButtonPress, name="Fecth Data from IP", when_pressed_function=self.btn_press, relx = 2)
+        
+        self.add(npyscreen.TitleText, w_id="city", name= "CITY:", begin_entry_at= 8, relx = 2)
+        self.add(npyscreen.ButtonPress, name="Fecth Data from city", when_pressed_function=self.btn_press_city, relx = 2)
+        self.add(npyscreen.ButtonPress, name = "Switch", when_pressed_function = self.btn_press_switch, relx = 2)
+
+
+    ## Fetch data from ip    
+    def btn_press(self):
+        w = Weather()
+        weather = w.getWeatherFromCityIP()
+        woeid = w.getWOEIDFromJSON(weather)
+        five_day_forecast = w.getWeatherForecast(woeid)
+
+        weather_today = five_day_forecast["consolidated_weather"][0]["weather_state_name"]
+        weather_tommorow = five_day_forecast["consolidated_weather"][1]["weather_state_name"]
+
+        weather_today_temperature = five_day_forecast["consolidated_weather"][0]["the_temp"]
+        weather_tommorow_temperature = five_day_forecast["consolidated_weather"][1]["the_temp"]
+
+
+        
+        npyscreen.notify_confirm("Weather today is " + weather_today + " and the temperature is " + str(weather_today_temperature)[:3] + "\n" + "Weather tommorow is " + weather_tommorow + " and the temperature is " + str(weather_tommorow_temperature)[:3], title="Weather is", wrap=True, wide=True, editw=1)
+    
+
+
+
+    ## City buttom press
+    def btn_press_city(self):
+        city = self.get_widget("city").value
+
+        if len(city) > 0:
+            w = Weather()
+            weather = w.getWeatherFromCity(city)
+            if weather and "title" in weather[0]:
+                woeid = w.getWOEIDFromJSON(weather)
+                five_day_forecast = w.getWeatherForecast(woeid)
+
+                weather_today = five_day_forecast["consolidated_weather"][0]["weather_state_name"]
+                weather_tommorow = five_day_forecast["consolidated_weather"][1]["weather_state_name"]
+
+                weather_today_temperature = five_day_forecast["consolidated_weather"][0]["the_temp"]
+                weather_tommorow_temperature = five_day_forecast["consolidated_weather"][1]["the_temp"]
+                
+                npyscreen.notify_confirm("Weather today is " + weather_today + " and the temperature is " + str(weather_today_temperature)[:3] + "\n" + "Weather tommorow is " + weather_tommorow + " and the temperature is " + str(weather_tommorow_temperature)[:3], title="Weather is", wrap=True, wide=True, editw=1)  
+            
+            else:
+                npyscreen.notify_confirm("Could not fetch data from that city", title="Weather is", wrap=True, wide=True, editw=1)  
+
+
+            
+        
+        else:
+            npyscreen.notify_confirm("You cannot input an empty string", title="Weather is", wrap=True, wide=True, editw=1)
+
+        
+
+
+
+
+    def btn_press_switch(self):
+        self.parentApp.switchForm('SECOND')
+
+    def on_ok(self):
+        self.parentApp.switchForm(None)
+
+class SecondForm(npyscreen.ActionFormMinimal):
+    def create(self):
+        pass
+
+        
+    def btn_press(self):
+        pass
+    
+    def btn_back(self):
+        self.parentApp.switchForm('MAIN')
+
+    def on_ok(self):
+        self.parentApp.switchForm(None)
+
+
+app = App()
+app.run()
---
+++ Weather.py
@@ -0,0 +1,88 @@
+import requests as re
+import json 
+import math
+
+## API used in the project
+## https://freegeoip.app/json/
+## https://ipinfo.io/161.185.160.93/
+## https://www.metaweather.com/api/location/search/?query=london
+## https://www.metaweather.com/api/location/554890/
+
+class Weather:
+    ## Get json weather data
+
+    def __init__(self):
+        self.ip_address = re.get(url = 'https://freegeoip.app/json')
+        self.ip_address_json = self.ip_address.json()
+
+
+    ## Different variables for the city##
+        self.ip_address_city = self.ip_address_json["city"]
+        self.ip_address_country = self.ip_address_json["country_name"]
+        self.ip_address_latlon = (self.ip_address_json["latitude"],self.ip_address_json["longitude"])
+
+
+
+    def getWeatherFromCityIP(self):
+        weatherData=re.get(url='https://www.metaweather.com/api/location/search/?query='+self.ip_address_city)
+        weather_data_json = weatherData.json()
+
+        return weather_data_json
+
+    def getWeatherFromCity(self,city):
+        weatherData=re.get(url='https://www.metaweather.com/api/location/search/?query='+city)
+        weather_data_json = weatherData.json()
+        return weather_data_json
+
+    ## Get Weather from lattlong
+    def getWeatherFromLattLong(self):
+        weatherData=re.get(url='https://www.metaweather.com/api/location/search/?lattlong='+str(self.ip_address_latlon[0])+','+str(self.ip_address_latlon[1]))
+        weather_data_json = weatherData.json()
+
+        return weather_data_json
+
+
+    ## Get WOEID from json
+    def getWOEIDFromJSON(self, weather_json):
+        woeid = weather_json[0]['woeid']
+    
+        return woeid
+
+
+    ## Get the weather 5 day forecast for the weather
+    def getWeatherForecast(self, woeid):
+        fiveDayForecast = re.get(url='https://www.metaweather.com/api/location/'+ str(woeid) +'/')
+        fiveDayForecast_json = fiveDayForecast.json()
+        return fiveDayForecast_json
+
+
+    def getNearestBrewery(self, city):
+        breweryData = re.get(url='https://api.openbrewerydb.org/breweries/search?query='+city)
+        brewery_data_json = breweryData.json()
+
+        return brewery_data_json
+
+
+    ## Save the IP 
+    def write_ip_to_file(self, json):
+        file = open('ip_address.txt', 'a')
+        file.write(json["ip"] + '\n')
+        file.close()
+
+
+    ## Returns the saved ip from the file
+    def get_ip_from_file(self):
+    
+        with open('ip_adress.txt') as f:
+            first_line = f.readline()
+
+        if len(first_line) < 0:
+            print("i got no ip")
+            return '-1'
+
+        else:
+            return first_line  
+    
+
+    ## Get the IP adress from the user ##
+    
